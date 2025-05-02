5b6ffc43eff62f57d8668d937343399e9611ad01
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2025-05-02 16:28:15 +0700

    update: Update SignInScreen.kt file

--- app/src/main/java/com/example/listify/presentation/ui/screen/auth/signin/SignInScreen.kt
+++ app/src/main/java/com/example/listify/presentation/ui/screen/auth/signin/SignInScreen.kt
@@ -1,9 +1,26 @@
 package com.example.listify.presentation.ui.screen.auth.signin
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun SignInScreen(modifier: Modifier = Modifier) {
+fun SignInScreen() {
     
+}
+
+@Composable
+fun SignInScreenContent(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+    )
+}
+
+@Preview(
+    showBackground = true,
+)
+@Composable
+private fun SignInScreenContentPreview() {
+    SignInScreenContent()
 }
