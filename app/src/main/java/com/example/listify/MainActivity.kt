fe35125113a58432214370e39e2d101a8aac4297
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2025-05-02 15:52:57 +0700

    fix: successfully add splash screen and fix bug action bar

--- app/src/main/java/com/example/listify/MainActivity.kt
+++ app/src/main/java/com/example/listify/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.example.listify.presentation.theme.ListifyTheme
 
@@ -11,6 +12,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
+
+        actionBar?.hide()
+
         setContent {
             ListifyTheme(dynamicColor = false) {
                 RootNavigationGraph(navController = rememberNavController())
