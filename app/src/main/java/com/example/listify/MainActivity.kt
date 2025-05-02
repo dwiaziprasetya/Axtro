19e6256ea2dc87dd886cf3206674a89aafa81a99
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2025-05-02 09:29:48 +0700

    update: rename ListifyApp.kt to RootNavigationGraph.kt file

--- app/src/main/java/com/example/listify/MainActivity.kt
+++ app/src/main/java/com/example/listify/MainActivity.kt
@@ -4,13 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.listify.presentation.theme.ListifyTheme
 
@@ -20,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ListifyTheme(dynamicColor = false) {
-                ListifyApp(navController = rememberNavController())
+                RootNavigationGraph(navController = rememberNavController())
             }
         }
     }
