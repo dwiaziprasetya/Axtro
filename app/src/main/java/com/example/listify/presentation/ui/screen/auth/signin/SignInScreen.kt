31712bfbb82a18b634dc52f9bc4fba4cb48f882a
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2025-05-03 16:35:56 +0700

    update: Update custom outline text field component

--- app/src/main/java/com/example/listify/presentation/ui/screen/auth/signin/SignInScreen.kt
+++ app/src/main/java/com/example/listify/presentation/ui/screen/auth/signin/SignInScreen.kt
@@ -53,6 +53,7 @@ import androidx.compose.ui.unit.sp
 import com.example.listify.R
 import com.example.listify.presentation.theme.ListifyTheme
 import com.example.listify.presentation.theme.poppinsFontFamily
+import com.example.listify.presentation.ui.component.CustomOutlinedTextField
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
@@ -308,85 +309,6 @@ fun SignInScreenContent(
             }
         }
     }
-}
-
-@Composable
-fun CustomOutlinedTextField(
-    value: String,
-    onValueChange: (String) -> Unit,
-    onFocusChange: (Boolean) -> Unit,
-    trailingIconResId: Int? = null,
-    onTrailingIconClick: () -> Unit,
-    visualTransformation: VisualTransformation = VisualTransformation.None,
-    isError: Boolean,
-    errorMessage: String,
-    hint: String = ""
-) {
-    val keyboardController = LocalSoftwareKeyboardController.current
-    var isFocused by remember { mutableStateOf(false) }
-
-    OutlinedTextField(
-        shape = RoundedCornerShape(10.dp),
-        colors = TextFieldDefaults.colors(
-            focusedContainerColor = MaterialTheme.colorScheme.background,
-            unfocusedContainerColor = MaterialTheme.colorScheme.background,
-            disabledContainerColor = MaterialTheme.colorScheme.background,
-            focusedIndicatorColor = MaterialTheme.colorScheme.primary,
-            unfocusedIndicatorColor = Color(0xFFCAC8C8),
-        ),
-        value = value,
-        onValueChange = onValueChange,
-        placeholder =  {
-            Text(
-                text = hint,
-                color = MaterialTheme.colorScheme.outline,
-                fontFamily = poppinsFontFamily,
-                fontSize = 14.sp
-            )
-        },
-        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
-        keyboardActions = KeyboardActions(onDone = {
-            keyboardController?.hide()
-        }),
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(50.dp)
-            .onFocusChanged {
-                isFocused = it.isFocused
-                onFocusChange(isFocused)
-            },
-        trailingIcon = {
-            if (trailingIconResId != null) {
-                Icon(
-                    painter = painterResource(trailingIconResId),
-                    contentDescription = null,
-                    tint = when {
-                        isError -> MaterialTheme.colorScheme.error
-                        isFocused -> MaterialTheme.colorScheme.primary
-                        else -> MaterialTheme.colorScheme.onPrimary
-                    },
-                    modifier = Modifier.clickable { onTrailingIconClick() }
-                )
-            }
-        },
-        visualTransformation = visualTransformation,
-        textStyle = TextStyle(
-            color = MaterialTheme.colorScheme.onPrimary,
-            fontFamily = poppinsFontFamily,
-            fontSize = 14.sp,
-            fontWeight = FontWeight.SemiBold
-        ),
-        isError = isError,
-        supportingText = {
-            if (isError) {
-                Text(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = errorMessage,
-                    color = MaterialTheme.colorScheme.error
-                )
-            }
-        }
-    )
 }
 
 @Preview(
