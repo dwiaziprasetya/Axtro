58240d08da1773e21e9787060a7c6e872b350a68
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2025-05-03 14:33:05 +0700

    update: Update SignInScreen.kt file

--- app/src/main/java/com/example/listify/presentation/ui/screen/auth/signin/SignInScreen.kt
+++ app/src/main/java/com/example/listify/presentation/ui/screen/auth/signin/SignInScreen.kt
@@ -4,23 +4,32 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.listify.R
 import com.example.listify.presentation.theme.ListifyTheme
+import com.example.listify.presentation.theme.poppinsFontFamily
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
@@ -47,7 +56,7 @@ fun SignInScreenContent(modifier: Modifier = Modifier) {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(300.dp)
+                .height(400.dp)
                 .background(MaterialTheme.colorScheme.primary)
                 .align(Alignment.TopCenter)
         )
@@ -56,6 +65,7 @@ fun SignInScreenContent(modifier: Modifier = Modifier) {
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .statusBarsPadding()
+                .padding(vertical = 16.dp)
                 .fillMaxWidth()
         ) {
             Image(
@@ -63,6 +73,34 @@ fun SignInScreenContent(modifier: Modifier = Modifier) {
                 contentDescription = "Logo",
                 modifier = Modifier
                     .size(60.dp)
+            )
+            Spacer(Modifier.height(16.dp))
+            Text(
+                text = "Sign in to your\naccount",
+                textAlign = TextAlign.Center,
+                color = Color.White,
+                fontSize = 32.sp,
+                lineHeight = 32.sp,
+                fontFamily = poppinsFontFamily,
+                fontWeight = FontWeight.SemiBold,
+            )
+            Spacer(Modifier.height(8.dp))
+            Text(
+                text = "Enter your email and password to sign in",
+                color = Color.White,
+                fontSize = 12.sp,
+                fontFamily = poppinsFontFamily,
+            )
+            Spacer(Modifier.height(32.dp))
+            Box(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .background(
+                        color = Color.White,
+                        shape = RoundedCornerShape(10.dp)
+                    )
+                    .fillMaxWidth()
+                    .height(300.dp)
             )
         }
     }
