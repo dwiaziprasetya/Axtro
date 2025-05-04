2f5ddc465630c99c9e3593f595515421d3ac30bb
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2025-05-04 23:37:07 +0700

    update: Modify SignUpScreen.kt fil

--- app/src/main/java/com/example/listify/presentation/ui/screen/auth/signup/SignUpScreen.kt
+++ app/src/main/java/com/example/listify/presentation/ui/screen/auth/signup/SignUpScreen.kt
@@ -2,9 +2,7 @@ package com.example.listify.presentation.ui.screen.auth.signup
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,13 +13,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
