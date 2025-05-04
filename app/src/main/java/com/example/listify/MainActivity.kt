659a31fcbd1a7f3556ab7caf5214ce58cf9cff3a
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2025-05-04 23:40:21 +0700

    update: Update MainActivity.kt file

--- app/src/main/java/com/example/listify/MainActivity.kt
+++ app/src/main/java/com/example/listify/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.listify
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
