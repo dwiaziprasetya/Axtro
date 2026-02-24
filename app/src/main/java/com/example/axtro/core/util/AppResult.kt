2ecaaa0d2a30a9504548545b15ff4b9e6cfbc386
dwiaziprasetya <dwiaziprasetya456@gmail.com>
2026-02-24 07:21:26 +0700

    update: Add AppResult.kt, User.kt and update Type.kt

---
+++ app/src/main/java/com/example/axtro/core/util/AppResult.kt
@@ -0,0 +1,7 @@
+package com.example.axtro.core.util
+
+sealed class AppResult<out T> {
+    data class Success<T>(val data: T) : AppResult<T>()
+    data class Error(val message: String, val throwable: Throwable? = null) : AppResult<Nothing>()
+    object Loading : AppResult<Nothing>()
+}
---
+++ app/src/main/java/com/example/axtro/domain/model/User.kt
@@ -0,0 +1,6 @@
+package com.example.axtro.domain.model
+
+data class User(
+    val id: String,
+    val email: String?
+)
