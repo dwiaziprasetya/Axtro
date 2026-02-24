2211a3ceed008ac2d50cce6206e2986d8157cdf3
dwiaziprasetya <dwiaziprasetya456@gmail.com>
2026-02-24 07:23:05 +0700

    feat: Add AuthRepository interface

---
+++ app/src/main/java/com/example/axtro/domain/repository/AuthRepository.kt
@@ -0,0 +1,15 @@
+package com.example.axtro.domain.repository
+
+import com.example.axtro.core.util.AppResult
+import com.example.axtro.domain.model.User
+
+interface AuthRepository {
+    suspend fun loginWithEmail(
+        email: String,
+        password: String
+    ) : AppResult<User>
+
+    suspend fun loginWithGoogle(
+        idToken: String
+    ) : AppResult<User>
+}
