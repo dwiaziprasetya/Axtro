d3fcb3342ff2b81ea8ab07b78415b99bf90e7b38
dwiaziprasetya <dwiaziprasetya456@gmail.com>
2026-03-16 17:21:57 +0700

    feat: Add getTasks function on task repository

--- app/src/main/java/com/example/axtro/domain/repository/TaskRepository.kt
+++ app/src/main/java/com/example/axtro/domain/repository/TaskRepository.kt
@@ -1,11 +1,16 @@
 package com.example.axtro.domain.repository
 
 import com.example.axtro.core.util.AppResult
+import com.example.axtro.domain.model.Task
+import kotlinx.coroutines.flow.Flow
 
 interface TaskRepository {
+
     suspend fun addTask(
         title: String,
         date: Long,
         priority: String
     ): AppResult<Unit>
+
+    fun getTasks() : Flow<List<Task>>
 }
