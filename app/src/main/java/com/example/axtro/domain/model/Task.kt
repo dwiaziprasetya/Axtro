c2aef8d58f51d604143acf2262a334265eba1b3b
dwiaziprasetya <dwiaziprasetya456@gmail.com>
2026-03-16 07:48:50 +0700

    feat: Add task data class

---
+++ app/src/main/java/com/example/axtro/domain/model/Task.kt
@@ -0,0 +1,10 @@
+package com.example.axtro.domain.model
+
+data class Task(
+    val id: String = "",
+    val title: String = "",
+    val date: Long = 0L,
+    val priority: String = "LOW",
+    val status: String = "ACTIVE",
+    val createdAt: Long = System.currentTimeMillis()
+)
--- app/src/main/java/com/example/axtro/domain/usecase/LoginWithGoogle.kt
+++ app/src/main/java/com/example/axtro/domain/usecase/LoginWithGoogle.kt
@@ -1,9 +1,7 @@
 package com.example.axtro.domain.usecase
 
 import com.example.axtro.core.util.AppResult
-import com.example.axtro.domain.model.User
 import com.example.axtro.domain.repository.AuthRepository
-import javax.inject.Inject
 
 class LoginWithGoogle (
     private val repository: AuthRepository
