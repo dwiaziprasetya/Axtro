b61d7a71a03ca723b6cbfd57fa1217b66ca40eef
dwiaziprasetya <dwiaziprasetya456@gmail.com>
2026-03-16 17:46:39 +0700

    update: Modify home ui state

--- app/src/main/java/com/example/axtro/presentation/home/HomeUiState.kt
+++ app/src/main/java/com/example/axtro/presentation/home/HomeUiState.kt
@@ -4,5 +4,7 @@ import com.example.axtro.domain.model.Task
 
 data class HomeUiState(
     val tasks: List<Task> = emptyList(),
-    val isLoading: Boolean = true
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val isSuccess: Boolean = false
 )
