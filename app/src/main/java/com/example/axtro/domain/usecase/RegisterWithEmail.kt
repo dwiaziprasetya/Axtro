ccaf1da4de2857beb4ca9c40f21f510a7cfec19d
dwiaziprasetya <dwiaziprasetya456@gmail.com>
2026-03-17 14:07:28 +0700

    feat: Add validation for register with email'

--- app/src/main/java/com/example/axtro/domain/usecase/RegisterWithEmail.kt
+++ app/src/main/java/com/example/axtro/domain/usecase/RegisterWithEmail.kt
@@ -13,11 +13,11 @@ class RegisterWithEmail(
     ): AppResult<User> {
 
         if (email.isBlank() || password.isBlank()) {
-            return AppResult.Error("Email & password tidak boleh kosong")
+            return AppResult.Error("Email and password cannot be empty")
         }
 
         if (password.length < 6) {
-            return AppResult.Error("Password minimal 6 karakter")
+            return AppResult.Error("Password must be at least 6 characters")
         }
 
         return repository.registerWithEmail(email, password)
