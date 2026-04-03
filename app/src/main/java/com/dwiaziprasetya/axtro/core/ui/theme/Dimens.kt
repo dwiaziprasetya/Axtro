70fadebc9857d5de1d6960068b786ac19a13513e
dwiaziprasetya <dwiaziprasetya456@gmail.com>
2026-04-03 21:07:32 +0700

    feat: Add dimension file

---
+++ app/src/main/java/com/dwiaziprasetya/axtro/core/ui/theme/Dimens.kt
@@ -0,0 +1,21 @@
+package com.dwiaziprasetya.axtro.core.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+object Dimens {
+    val PaddingSmall = 8.dp
+    val PaddingMedium = 16.dp
+    val PaddingLarge = 32.dp
+
+    val ButtonHeight = 55.dp
+    val CornerRadius = 10.dp
+
+    val TextSmall = 12.sp
+    val TextMedium = 14.sp
+    val TextLarge = 32.sp
+
+    val SpacerSmall = 4.dp
+    val SpacerMedium = 8.dp
+    val SpacerLarge = 16.dp
+}
